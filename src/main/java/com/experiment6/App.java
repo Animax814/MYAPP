e18862b7c9555393a1a727100219b6ad0ea7d0e3
anil <aaniln053@gmail.com>
2026-06-17 11:42:39 +0530

    Initialised MAVEN project

---
+++ src/main/java/com/experiment6/App.java
@@ -0,0 +1,13 @@
+package com.experiment6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello jenkins CI" );
+    }
+}
